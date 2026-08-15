1ea923151333c6e6746c73648578b721d9667362
vishwa subash <subramanisubash2004@gmail.com>
2026-08-15 17:04:37 +0530

    Add Vercel URL to CORS allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import auth,resume,analysis
+from app.routers import auth, resume, analysis
 
 app = FastAPI(title="AI Resume Analyzer")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "ai-resume-analyzer-cyan-psi.vercel.app",  # replace with your actual Vercel URL
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -15,6 +18,7 @@ app.add_middleware(
 app.include_router(auth.router)
 app.include_router(resume.router)
 app.include_router(analysis.router)
+
 
 @app.get("/")
 def read_root():
